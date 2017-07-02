5bac03f7e5415d7fa33a1ff264a18cda32828eba
Leandro Macedo <contato@leandromacedo.com>
2017-07-02 13:44:15 +0100

    Include MaterializeCSS in Website

---
+++ app/controllers/page_controller.rb
@@ -0,0 +1,16 @@
+class PageController < ApplicationController
+
+  layout 'site'
+
+  def home
+  end
+
+  def about
+  end
+
+  def service
+  end
+
+  def contact
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  root 'page#home'
+  get 'page/home'
+
+  get 'page/about'
+
+  get 'page/service'
+
+  get 'page/contact'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
