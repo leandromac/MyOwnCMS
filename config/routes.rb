00ddfaece1c895127654da7bced59def7643cc1f
Leandro Macedo <contato@leandromacedo.com>
2017-07-06 20:08:22 +0100

    Custom login panel

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   end
   get 'panel/home'
 
-  devise_for :users
+  devise_for :users, controllers: { sessions: 'users/sessions' }
   namespace :panel do
     resources :services
   end
